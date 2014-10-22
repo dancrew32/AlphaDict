1d2708aa5ad0f92e3a8efc40fe0bb185f67c53ad
Dan Masquelier <dancrew32@gmail.com>
2014-10-22 10:16:00 -0700

    init AlphaDict repo

---
+++ README.md
@@ -0,0 +1,16 @@
+# AlphaDict
+
+Recursively sorts dictionaries by key. 
+
+Useful for determining if the hash of two dicts that contain identical data,
+but are in slightly different orders, are the same.
+
+Experienced this pain point when trying to quickly determine if a JSON object 
+from the client had ever been previously cached by our search engine. 
+The order of the JSON would vary slightly, but the desired data could often be the same.
+
+Check out some of the examples in the unit tests:
+
+```bash
+python -m unittest discover
+```
---
+++ test_alpha_dict.py
@@ -0,0 +1,218 @@
+from unittest import TestCase
+from collections import OrderedDict
+from alpha_dict import AlphaDict
+from random import choice
+import string
+
+
+class AlphaDictTestCase(TestCase):
+
+    def test_correctly_sorted(self):
+
+        guts = (
+            ('a', choice(string.digits)),
+            ('b', choice(string.digits)),
+            ('c', choice(string.digits)),
+            ('thing', OrderedDict((
+                ('a', choice(string.digits)),
+                ('b', choice(string.digits)),
+                ('c', choice(string.digits)),
+                ('d', [
+                    OrderedDict((
+                        ('a', choice(string.digits)),
+                        ('b', choice(string.digits)),
+                        ('c', choice(string.digits)),
+                    )),
+                    OrderedDict((
+                        ('a', choice(string.digits)),
+                        ('b', choice(string.digits)),
+                        ('c', choice(string.digits)),
+                    )),
+                    OrderedDict((
+                        ('a', choice(string.digits)),
+                        ('b', choice(string.digits)),
+                        ('c', choice(string.digits)),
+                    )),
+                    [
+                        OrderedDict((
+                            ('a', choice(string.digits)),
+                            ('b', choice(string.digits)),
+                            ('c', choice(string.digits)),
+                        ))
+                    ],
+                    [
+                        OrderedDict((
+                            ('a', choice(string.digits)),
+                            ('b', choice(string.digits)),
+                            ('c', choice(string.digits)),
+                        ))
+                    ],
+                    ]),
+                ('thing', OrderedDict((
+                    ('a', choice(string.digits)),
+                    ('b', choice(string.digits)),
+                    ('c', choice(string.digits)),
+                )))
+            )))
+        )
+
+        self.assertEqual(AlphaDict(guts), OrderedDict(guts))
+
+    def test_incorrectly_sorted(self):
+        guts = (
+            ('a', choice(string.digits)),
+            ('b', choice(string.digits)),
+            ('c', choice(string.digits)),
+            ('thing', OrderedDict((
+                ('a', choice(string.digits)),
+                ('b', choice(string.digits)),
+                ('c', choice(string.digits)),
+                ('d', [
+                    OrderedDict((
+                        ('a', choice(string.digits)),
+                        ('b', choice(string.digits)),
+                        ('c', choice(string.digits)),
+                    )),
+                    OrderedDict((
+                        ('a', choice(string.digits)),
+                        ('b', choice(string.digits)),
+                        ('c', choice(string.digits)),
+                    )),
+                    OrderedDict((
+                        ('a', choice(string.digits)),
+                        ('b', choice(string.digits)),
+                        ('c', choice(string.digits)),
+                    )),
+                    [
+                        OrderedDict((
+                            ('a', choice(string.digits)),
+                            ('b', choice(string.digits)),
+                            ('c', choice(string.digits)),
+                        ))
+                    ],
+                    [
+                        OrderedDict((
+                            ('b', choice(string.digits)),
+                            ('a', choice(string.digits)),
+                            ('c', choice(string.digits)),
+                        )),
+                        OrderedDict((
+                            ('a', choice(string.digits)),
+                            ('b', choice(string.digits)),
+                            ('c', choice(string.digits)),
+                        ))
+                    ],
+                ]),
+                ('thing', OrderedDict((
+                    ('a', choice(string.digits)),
+                    ('b', choice(string.digits)),
+                    ('c', choice(string.digits)),
+                )))
+            )))
+        )
+        self.assertNotEqual(AlphaDict(guts), OrderedDict(guts))
+
+    def test_first_level_correction(self):
+        a = AlphaDict((
+            ('b', 1),
+            ('a', 1),
+            ('c', 1),
+        ))
+        b = OrderedDict((
+            ('a', 1),
+            ('b', 1),
+            ('c', 1),
+        ))
+        self.assertEqual(a, b)
+
+    def test_second_level_correction(self):
+        a = AlphaDict((
+            ('b', 1),
+            ('a', OrderedDict((
+                ('b', 1),
+                ('a', 1),
+                ('c', 1),
+            ))),
+            ('c', 1),
+        ))
+        b = OrderedDict((
+            ('a', OrderedDict((
+                ('a', 1),
+                ('b', 1),
+                ('c', 1),
+            ))),
+            ('b', 1),
+            ('c', 1),
+        ))
+        self.assertEqual(a, b)
+
+    def test_list_dict_correction(self):
+        a = AlphaDict((
+            ('a', [
+                OrderedDict((
+                    ('b', 1),
+                    ('a', 1),
+                    ('c', 1),
+                )),
+                OrderedDict((
+                    ('c', 1),
+                    ('a', 1),
+                    ('b', 1),
+                )),
+                OrderedDict((
+                    ('a', 1),
+                    ('b', 1),
+                    ('c', 1),
+                ))
+            ]),
+        ))
+        b = OrderedDict((
+            ('a', [
+                OrderedDict((
+                    ('a', 1),
+                    ('b', 1),
+                    ('c', 1),
+                )),
+                OrderedDict((
+                    ('a', 1),
+                    ('b', 1),
+                    ('c', 1),
+                )),
+                OrderedDict((
+                    ('a', 1),
+                    ('b', 1),
+                    ('c', 1),
+                ))
+            ]),
+        ))
+        self.assertEqual(a, b)
+
+    def test_key_delete(self):
+        a = AlphaDict((
+            ('a', [
+                OrderedDict((
+                    ('a', 1),
+                    ('b', 1),
+                    ('c', 1),
+                )),
+            ]),
+            ('b', [
+                OrderedDict((
+                    ('b', 1),
+                    ('a', 1),
+                    ('c', 1),
+                )),
+            ]),
+        ))
+        del a['a']
+        b = OrderedDict((
+            ('b', [
+                OrderedDict((
+                    ('a', 1),
+                    ('b', 1),
+                    ('c', 1),
+                )),
+            ]),
+        ))
+        self.assertEqual(a, b)
+
