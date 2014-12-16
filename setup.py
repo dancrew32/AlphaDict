fc28e6cc94a1e0fe798aa1c0b1fb9daa6084a804
Dan Masquelier <dancrew32@gmail.com>
2014-12-16 14:07:21 -0800

    remove egg info from repo

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='alphadict',
-      version='1.0',
+      version='1.02',
       description='Recursively sorts dictionaries by key.',
       url='https://github.com/dancrew32/AlphaDict',
       author='Dan Masquelier',
       author_email='dmasquelier@gmail.com',
       license='MIT',
+      include_package_data=True,
       packages=['alphadict'],
       zip_safe=False)
